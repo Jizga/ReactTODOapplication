e60b1d6cd772573e5f67f8a67c77e65f9d4f0493
Jizga <jizga93@gmail.com>
2021-06-15 11:02:22 +0000

    Task component's architecture improved

--- src/js/component/Task.js
+++ src/js/component/Task.js
@@ -4,12 +4,18 @@ import PropTypes from "prop-types";
 export function Task(props) {
 	return (
 		<div className="row">
-			<div className="col-5 col-sm-6 col-md-6 col-lg-8 col-xl-8 bg-danger">
-				{props.taskText}
-			</div>
-			<div className="col-1 col-sm-1 col-md-1 col-lg-2 col-xl-2 bg-warning">
-				<i class="far fa-trash-alt"></i>
-			</div>
+			{props.taskText ? (
+				<div className="col-5 col-sm-6 col-md-6 col-lg-8 col-xl-8">
+					{props.taskText}
+					<hr></hr>
+				</div>
+			) : null}
+			{props.taskText ? (
+				<div className="col-1 col-sm-1 col-md-1 col-lg-2 col-xl-2">
+					<i className="far fa-trash-alt"></i>
+					<hr></hr>
+				</div>
+			) : null}
 		</div>
 	);
 }
--- src/js/component/TasksList.js
+++ src/js/component/TasksList.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task } from "./Task";
 
 export function TaskList() {
+	const [newTask, setNewTask] = useState("");
+
+	const tasks = [
+		{
+			id: 1,
+			text: "Learn React"
+		},
+		{
+			id: 2,
+			text: "Learn Python"
+		}
+		// {
+		// 	id: new Date().getMilliseconds(),
+		// 	text: newTask
+		// }
+	];
+
+	// let list = tasks.map(t => {
+	// 	console.log("task ", t, typeof t);
+	// 	return <Task key={t.id} taskText={t.text} />;
+	// });
+
+	let addNewTask = [
+		...tasks,
+		{ id: new Date().getMilliseconds(), text: newTask }
+	];
+
+	let list = addNewTask.map(t => {
+		console.log("task ", t, typeof t);
+		return <Task key={t.id} taskText={t.text} />;
+	});
+
 	return (
 		<div className="container text-center mt-5 myListContainer">
-			<Task taskText="prueba 1" />
+			<div className="row d-flex flex-column">
+				<h1 className="col-6 col-sm-7 col-md-7 col-lg-9 col-xl-9 mb-3">
+					TO DO App
+				</h1>
+				<div className="row mb-3">
+					<input
+						className="col-6 col-sm-7 col-md-7 col-lg-10 col-xl-10"
+						type="text"
+						onChange={e => setNewTask(e.target.value)}
+						value={newTask}
+						placeholder="No tasks, add a task"
+					/>
+				</div>
+				<div>{list}</div>
+			</div>
 		</div>
 	);
 }
